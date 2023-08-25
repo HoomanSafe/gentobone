72126e28d6e2dc9c47e220665086db1298a724fc
Arc <atandarokib@gmail.com>
2023-08-25 20:24:42 +0100

    up

--- src/Components/Token.jsx
+++ src/Components/Token.jsx
@@ -7,8 +7,8 @@ const Token = () => {
       <div className="container flex justify-center items-center text-center flex-col gap-7">
         <h1 className="text-5xl text-white uppercase font-bold">tokenomics</h1>
         <p className="text-xl md:text-2xl mt-20">1,000,000,000 supply</p>
-        <p className="text-xl md:text-2xl">0% buy tax</p>
-        <p className="text-xl md:text-2xl">0% sell tax</p>
+        <p className="text-xl md:text-2xl">1% buy tax</p>
+        <p className="text-xl md:text-2xl">1% sell tax</p>
 
         {/* <div id="dexscreener-embed" className="mt-32">
           <iframe src="https://dexscreener.com/ethereum/0x687f721fbd06647255831EF17c455435966D0170?embed=1&theme=dark&trades=0&info=0"></iframe>
