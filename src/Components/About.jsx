620c832596da01521a47f6b76b495425c04ff9d5
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-10 15:14:19 +0800

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,31 +1,24 @@
 import React from "react";
-import Pepe from "../assets/pepe.png";
-import Pepe1 from "../assets/1.png";
+import Pepe from "../assets/logo1.png";
+import Pepe1 from "../assets/banner1.jpeg";
 
 const About = () => {
   return (
     <section className="py-[50px] relative w-full bg-red">
       <div className="container flex justify-center items-center text-center flex-col gap-7">
         <h1 className="text-5xl text-primary uppercase font-bold">
-          about rare pepe
+          about Gento Bone
         </h1>
         <img src={Pepe1} alt="" className="w-[15rem] right-0 bottom-0" />
 
         <p className="text-xl md:text-2xl mt-10">
-          Picture this: Rare Pepe, a digital currency that trades in giggles
-          rather than dollars. Instead of serious financial transactions, users
-          engage in uproarious exchanges of rare memes that'll leave your sides
-          splitting. These one-of-a-kind tokens represent the pinnacle of meme
-          artistry, featuring Pepe the Frog in the most outrageous and
-          sidesplitting scenarios you can imagine.
+          $GENTO is not your average coin meme. 
+          $GENTO is a meme with a limited supply and continues to circulate.
         </p>
 
         <p className="text-xl md:text-2xl mt-10">
-          How does it work? Well, imagine stumbling upon a Rare Pepe token
-          depicting a majestic Pepe in a tutu, gracefully pirouetting atop a
-          banana peel. Or perhaps you'll find a limited edition Rare Pepe where
-          Pepe's face is hilariously morphed with that of a rubber chicken. The
-          possibilities are as endless as the internet's absurdity!
+          The main principle of design is that the output produced must not cause inflation. Instead, results are obtained from real activities that produce positive reinforcement that organically sustains the ecosystem.
+          Combined with an updated decentralized governance structure, improved tokenomics and automatic burning.
         </p>
         <img
           src={Pepe}
