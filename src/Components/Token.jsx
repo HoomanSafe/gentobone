b4a8a3475d3e8ba9893d8434a2c1e7b9eb5bf9ad
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-10 15:37:16 +0800

    Update Token.jsx

--- src/Components/Token.jsx
+++ src/Components/Token.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Pepe from "../assets/pepe2.png";
+import Pepe from "../assets/logo1.png";
 
 const Token = () => {
   return (
     <section className="py-[50px] w-full">
       <div className="container flex justify-center items-center text-center flex-col gap-7">
         <h1 className="text-5xl text-white uppercase font-bold">tokenomics</h1>
-        <p className="text-xl md:text-2xl mt-20">1,000,000,000 supply</p>
-        <p className="text-xl md:text-2xl">1% buy tax</p>
-        <p className="text-xl md:text-2xl">1% sell tax</p>
+        <p className="text-xl md:text-2xl mt-20">2,000 Tsupply</p>
+        <p className="text-xl md:text-2xl">5% buy tax</p>
+        <p className="text-xl md:text-2xl">5% sell tax</p>
 
         {/* <div id="dexscreener-embed" className="mt-32">
           <iframe src="https://dexscreener.com/ethereum/0x687f721fbd06647255831EF17c455435966D0170?embed=1&theme=dark&trades=0&info=0"></iframe>
